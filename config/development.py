6aff25e869a3bc319e20fb587444e407a83291c5
John Paulo Panganiban <olopangs@gmail.com>
2021-03-24 06:03:36 +0800

    make it better

--- config/development.py
+++ config/development.py
@@ -5,17 +5,22 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['oloproject.eba-mdgm24am.us-west-2.elasticbeanstalk.com']
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('RDS_DB_NAME'),
+#         'USER': os.environ.get('RDS_USERNAME'),
+#         'PASSWORD': os.environ.get('RDS_PASSWORD'),
+#         'HOST': os.environ.get('RDS_HOSTNAME'),
+#         'PORT': os.environ.get('RDS_PORT')
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('RDS_DB_NAME'),
-        'USER': os.environ.get('RDS_USERNAME'),
-        'PASSWORD': os.environ.get('RDS_PASSWORD'),
-        'HOST': os.environ.get('RDS_HOSTNAME'),
-        'PORT': os.environ.get('RDS_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
