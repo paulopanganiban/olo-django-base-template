6a96c405563ca43bfbc783295de1126c6a601f49
John Paulo Panganiban <olopangs@gmail.com>
2021-03-28 23:21:43 +0800

    meh

--- config/development.py
+++ config/development.py
@@ -5,7 +5,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['oloproject.eba-mdgm24am.us-west-2.elasticbeanstalk.com']
 SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = '=+zl2)zrcv=dduc1@bw)+ab)6x8)97nm**i!vruv)oal@$y4&!'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -16,18 +15,7 @@ if 'RDS_DB_NAME' in os.environ:
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
         }
-    } 
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': config('DB_NAME'),
-#             'USER': config('DB_USER'),
-#             'PASSWORD': config('DB_PASSWORD'),
-#             'HOST': config('DB_HOST'),
-#             'PORT': config('DB_PORT'),
-#         }
-#     }
+    }
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
