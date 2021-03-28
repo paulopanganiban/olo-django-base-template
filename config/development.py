ca469aa9bdced97056e936f1ae85e37dbe4cfea4
John Paulo Panganiban <olopangs@gmail.com>
2021-03-28 22:37:15 +0800

    added RDS live server and connected to local PgAdmin

--- config/development.py
+++ config/development.py
@@ -1,11 +1,11 @@
 from .base import *
 import os
-
+from decouple import config
 DEBUG = True
 
 ALLOWED_HOSTS = ['oloproject.eba-mdgm24am.us-west-2.elasticbeanstalk.com']
-SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = '=+zl2)zrcv=dduc1@bw)+ab)6x8)97nm**i!vruv)oal@$y4&!'
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '=+zl2)zrcv=dduc1@bw)+ab)6x8)97nm**i!vruv)oal@$y4&!'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -16,7 +16,19 @@ if 'RDS_DB_NAME' in os.environ:
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
         }
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
     }
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
