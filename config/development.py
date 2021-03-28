cf549dc0a2eac476c68baf995b70ca94e8c3a0c9
John Paulo Panganiban <olopangs@gmail.com>
2021-03-29 05:27:32 +0800

    mehh

--- config/development.py
+++ config/development.py
@@ -1,11 +1,10 @@
 from .base import *
 import os
-# from decouple import config
 DEBUG = True
 
 ALLOWED_HOSTS = ['oloproject.eba-mdgm24am.us-west-2.elasticbeanstalk.com', '127.0.0.1']
 SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = config('SECRET_KEY')
+
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -17,17 +16,6 @@ if 'RDS_DB_NAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': config('DB_NAME'),
-#             'USER': config('DB_USER'),
-#             'PASSWORD': config('DB_PASSWORD'),
-#             'HOST': config('DB_HOST'),
-#             'PORT': config('DB_PORT'),
-#         }
-#     }
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
--- config/local.py
+++ config/local.py
@@ -17,3 +17,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+# SECRET_KEY = config('SECRET_KEY')
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': config('DB_HOST'),
+#             'PORT': config('DB_PORT'),
+#         }
+#     }
